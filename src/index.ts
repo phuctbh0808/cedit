f201e4877b544ad65702e1f6e50fc8a6ae3af244
phongtn3 <phongtn@remitano.com>
2023-10-21 20:53:58 +0700

    feat(lend): fix add reserve cmd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
 import { Command } from "commander";
 import * as anchor from "@project-serum/anchor";
 import { exec } from "child_process";
@@ -9,11 +8,6 @@ import { getOrCreateAssociatedTokenAccount } from "spl-token";
 
 const program = new Command();
 
-const __path = process.cwd();
-const ownerKey = JSON.parse(fs.readFileSync(__path + "/.wallets/owner.json"));
-const reUSD = new PublicKey("4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3");
-const reBTC = new PublicKey("GwPQTMg3eMVpDTEE3daZDtGsBtNHBK3X47dbBJvXUzF4");
-const reETH = new PublicKey("GwGh3b7iNibT3gpGn6SwZA9xZme7Th4NZmuGVD75jpZL");
 const opts: anchor.web3.ConfirmOptions = {
   commitment: "confirmed",
 };
@@ -21,12 +15,13 @@ const opts: anchor.web3.ConfirmOptions = {
 program
   .command("add-reserve")
   .description("Add new reserve")
-  .option("--program-id <string>", "")
-  .option("--owner <string>", "")
-  .option("--market-owner <string>", "")
+  .option("--program_id <string>", "")
+  .option("--payer <string>", "")
+  .option("--market_owner <string>", "")
   .option("--token_sympol <string>", "Token symbol: ReUSD, RENEC, ReBTC, ReETH")
   .option("--amount <number>", "")
-  .option("--network-url <string>", "")
+  .option("--cluster <string>", "")
+  .option("--network_url <string>", "")
   .option("--market_addr <string>", "Market address")
   .option("--optimal_utilization_rate <number>", "")
   .option("--max_utilization_rate <number>", "")
@@ -44,14 +39,15 @@ program
   .option("--host_fee_percentage <number>", "")
   .option("--deposit_limit <number>", "")
   .option("--added_borrow_weight_bps <number>", "")
-  .option("--protocol_take_rate")
+  .option("--protocol_take_rate <number>", "")
   .action(async (params) => {
     let {
       program_id,
-      owner,
+      payer,
       market_owner,
       token_sympol,
       amount,
+      cluster,
       network_url,
       market_addr,
       optimal_utilization_rate,
@@ -73,52 +69,90 @@ program
       protocol_take_rate,
     } = params;
 
-    console.log("Add new reserve");
-    console.log("params:", params);
-    const connection = new Connection(network_url, opts);
-    const keypair = Keypair.fromSecretKey(ownerKey);
-    let tokenAccount = owner;
-    let tokenProgramId = reUSD;
-    switch (token_sympol.toUpperCase()) {
-      case "ReBTC":
-        tokenProgramId = reBTC;
-        break;
-      case "ReETH":
-        tokenProgramId = reETH;
-        break;
+    let reUSD = new PublicKey("USDbgSB1DPBCEDt15ppgTZZYiQokMHcvkX6JQRfVNJY");
+    let reBTC = new PublicKey("BTCxWeEcT5sYsys7ncnr2bU1Mzbn1aXXqJDv43X22cDy");
+    let reETH = new PublicKey("ETH6nBodGWYQxz5qZ6C94E4DdQuH5iGTSwdhLHgiLzRy");
+    if (cluster == "mainnet") {
+      reUSD = new PublicKey("4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3");
+      reBTC = new PublicKey("GwPQTMg3eMVpDTEE3daZDtGsBtNHBK3X47dbBJvXUzF4");
+      reETH = new PublicKey("GwGh3b7iNibT3gpGn6SwZA9xZme7Th4NZmuGVD75jpZL");
     }
 
-    if (token_sympol.toUpperCase() != "RENEC") {
-      tokenAccount = (
-        await getOrCreateAssociatedTokenAccount(connection, keypair, tokenProgramId, owner)
-      ).address.toBase58();
-    }
+    const caseUSD = "reusdmainnet";
+    const caseBTC = "rebtcmainnet";
+    const caseETH = "reethmainnet";
+    const caseRenec = "renecmainnet";
+    const caseUSD_test = "reusdtestnet";
+    const caseBTC_test = "rebtcmainnet";
+    const caseETH_test = "reethmainnet";
+    const caseRenec_test = "renectestnet";
+    const tokenCase = token_sympol.toLowerCase() + cluster.toLowerCase();
 
     let oracleProduct = "";
     let oraclePrice = "";
-    switch (token_sympol.toUpperCase()) {
-      case "ReBTC":
+    switch (tokenCase) {
+      case caseBTC:
         oracleProduct = "4kn3JeaXhBbbwKF3kaYubiWHVCxkJdJap6zsm54CS4YQ";
         oraclePrice = "EWMPWwzt5wFWmtsYhEvGxZ8EvEsPp9Fm5rfQBwoXR3Nz";
         break;
-      case "ReETH":
+      case caseETH:
         oracleProduct = "BKcFo4FD6jSjttHdfwPGuBVZWhZXfSCU6KEFJhhTija1";
         oraclePrice = "CeVUAv8Fk7XfyVefBSZvpQZMMdkBanL4k4jLNCCjwrNC";
         break;
-      case "RENEC":
+      case caseRenec:
         oracleProduct = "8jMh1d8NA84AZErW2uZ71Jbhci3PyV3WaySbVVLXGctt";
         oraclePrice = "nakiaURmuNcNS1nUwJzqKWU1x9CPXXfK4xVSK1utcL7";
         break;
-      case "ReUSD":
+      case caseUSD:
+        oracleProduct = "4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3";
+        oraclePrice = "4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3";
+        break;
+      case caseBTC_test:
+        oracleProduct = "4kn3JeaXhBbbwKF3kaYubiWHVCxkJdJap6zsm54CS4YQ";
+        oraclePrice = "EWMPWwzt5wFWmtsYhEvGxZ8EvEsPp9Fm5rfQBwoXR3Nz";
+        break;
+      case caseETH_test:
+        oracleProduct = "BKcFo4FD6jSjttHdfwPGuBVZWhZXfSCU6KEFJhhTija1";
+        oraclePrice = "CeVUAv8Fk7XfyVefBSZvpQZMMdkBanL4k4jLNCCjwrNC";
+        break;
+      case caseRenec_test:
+        oracleProduct = "8jMh1d8NA84AZErW2uZ71Jbhci3PyV3WaySbVVLXGctt";
+        oraclePrice = "nakiaURmuNcNS1nUwJzqKWU1x9CPXXfK4xVSK1utcL7";
+        break;
+      case caseUSD_test:
         oracleProduct = "4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3";
         oraclePrice = "4Q89182juiadeFgGw3fupnrwnnDmBhf7e7fHWxnUP3S3";
         break;
     }
 
+    console.log("Add new reserve");
+    console.log("params:", params);
+    const connection = new Connection(network_url, opts);
+    const keypair = Keypair.fromSecretKey(Uint8Array.from(payer));
+    let tokenAccount = keypair.publicKey.toBase58(); // ATA of source owner which is payer
+    let tokenProgramId = reUSD;
+    switch (token_sympol.toUpperCase()) {
+      case "REBTC":
+        tokenProgramId = reBTC;
+        break;
+      case "REETH":
+        tokenProgramId = reETH;
+        break;
+    }
+
+    if (token_sympol.toUpperCase() != "RENEC") {
+      const ata = await getOrCreateAssociatedTokenAccount(
+        connection,
+        keypair,
+        tokenProgramId,
+        new PublicKey(tokenAccount),
+      );
+      tokenAccount = ata.address.toBase58();
+    }
+
     const exeParams = [
-      `--program ${program_id}`,
       `--market-owner ${market_owner}`,
-      `--source-owner ${ownerKey}`,
+      `--source-owner ${payer}`,
       `--market ${market_addr}`,
       `--source ${tokenAccount}`,
       `--amount ${amount}`,
@@ -146,7 +180,7 @@ program
       "--verbose",
     ];
 
-    let exeCmd = `target/debug/relend-program add-reserve ` + exeParams.join(" ");
+    let exeCmd = `target/debug/relend-program --program ${program_id} add-reserve ` + exeParams.join(" ");
     exec(exeCmd, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
