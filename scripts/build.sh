30cf286bc91f46e9b37abd7049ba66517dcbb6db
phongtn3 <phongtn@remitano.com>
2023-10-21 19:22:36 +0700

    feat(lend): update build script with anchor structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,24 @@ PROGRAM_NAME_UNDERSCORE=${PROGRAM_NAME//-/_}
 if [[ -n $1 ]]; then
     PROGRAM_ID=$1
 else
-    PROGRAM_ID_FILE="token-lending/program/program-id.md"
-    PROGRAM_ID=$(cat $PROGRAM_ID_FILE) # Load content from file
+    anchor keys list
+    PROGRAM_ID=$(solana address -k target/deploy/$PROGRAM_NAME_UNDERSCORE-keypair.json)
 fi
 echo "PROGRAM_ID: $PROGRAM_ID"
+
+# Set program id to anchor config for consistency
+ANCHOR_FILE_PATH="programs/relend_program/src/lib.rs"
+# Replace the existing declare_id! line with the new PROGRAM_ID
+TEMP_FILE=$(mktemp)
+sed "s/solana_program::declare_id!([^)]*)/solana_program::declare_id!(\"$PROGRAM_ID\")/" "$ANCHOR_FILE_PATH" > "$TEMP_FILE"
+cat "$TEMP_FILE" > "$ANCHOR_FILE_PATH"
+rm "$TEMP_FILE"
+
+
+# Set program id to md file
+PROGRAM_ID_FILE="token-lending/program/program-id.md"
+echo $PROGRAM_ID > "$PROGRAM_ID_FILE"
+
 
 # Set the file path
 FILE_PATH="token-lending/program/src/lib.rs"
