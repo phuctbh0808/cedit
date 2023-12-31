0cc0cd27dbb47163bc76572b14f29474f4991cd3
phongtn3 <phongtn@remitano.com>
2023-12-31 15:03:31 +0700

    feat(bte): add borrow to earn program

---
+++ reearn/programs/reearn_program/src/lib.rs
@@ -0,0 +1,60 @@
+use anchor_lang::prelude::*;
+
+declare_id!("J1zcRDYPC3Rooc3BwenwBfYHubRG3JW45C9FPjY8ayX3");
+
+pub mod constants;
+pub mod reward_token;
+pub mod errors;
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+
+#[program]
+pub mod reearn_program {
+
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        bump: u8,
+        vault_bump: u8,
+        operator: Pubkey,
+    ) -> ProgramResult {
+        initialize::exec(ctx, bump, vault_bump, operator)
+    }
+
+    pub fn change_operator(ctx: Context<ChangeOperator>, new_operator: Pubkey) -> ProgramResult {
+        change_operator::exec(ctx, new_operator)
+    }
+
+    pub fn change_lock_duration(ctx: Context<ChangeLockDuration>, duration: u32) -> ProgramResult {
+        change_lock_duration::exec(ctx, duration)
+    }
+
+    pub fn refresh_reward(
+        ctx: Context<RefreshReward>,
+        obligation: Pubkey,
+        wallet: Pubkey,
+        amount: u64,
+        expo: i8,
+    ) -> ProgramResult {
+        refresh_reward::exec(ctx, obligation, wallet, amount, expo)
+    }
+
+    pub fn register_for_earn(
+        ctx: Context<RegisterForEarn>,
+        obligation: Pubkey,
+        wallet: Pubkey,
+    ) -> ProgramResult {
+        register_for_earn::exec(ctx, obligation, wallet)
+    }
+
+    pub fn claim_reward(ctx: Context<ClaimReward>, obligation: Pubkey, wallet: Pubkey) -> ProgramResult {
+        claim_reward::exec(ctx, obligation, wallet)
+    }
+
+    pub fn supply(ctx: Context<SupplyReward>, amount: u64) -> ProgramResult {
+        supply::exec(ctx, amount)
+    }
+}
