c8be44b355ed1ac42d85c98f668709f96676a475
phongtn3 <phongtn@remitano.com>
2024-01-03 15:17:49 +0700

    clean up

--- programs/reearn_program/src/constants.rs
+++
@@ -1,3 +0,0 @@
-pub const CONFIG_SEED: &[u8] = b"supernova";
-pub const OBLIGATION_REWARD_SEED: &[u8] = b"tothemoon";
-pub const VAULT_SEED: &[u8] = b"onepiece";
--- programs/reearn_program/src/state/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod borrow_to_earn;
-pub mod config;
-
-pub use borrow_to_earn::*;
-pub use config::*;
