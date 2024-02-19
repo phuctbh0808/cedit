4dc8bfe0883a185e20a05c4bb96e409d4d1f689a
phongtn3 <phongtn@remitano.com>
2024-02-19 21:21:25 +0700

    feat(ste): fix empty reward

--- reearn/programs/reearn_program/src/state/supply_to_earn.rs
+++ reearn/programs/reearn_program/src/state/supply_to_earn.rs
@@ -28,11 +28,10 @@ impl SupplyApy {
     pub const LEN: usize = 8 + 32 + 4 + 32 + 1 + 1;
 
     pub fn calculate_reward(&self, supply_amount: u64, duration: i64) -> f64 {
-        let time_diff_years = duration / (365 * 24 * 60 * 60);
+        let time_diff_years = (duration as f64) / 31536000_f64; // 60*60*24*365
         let principal = supply_amount as f64;
         let compound_interest = (self.apy + 1.0) as f64;
-        let compounded = (principal * compound_interest)*time_diff_years as f64;
-        let earnings = compounded - principal;
+        let earnings = principal * compound_interest*time_diff_years;
         earnings
     }
 }
